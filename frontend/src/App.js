41d136d71aeda136d1bff0e3db6da7149b40e2d8
Mujtaba Shafqat <mshafqat.bscs21seecs@seecs.edu.pk>
2024-09-07 13:38:03 +0500

    Added signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Homepage from "./pages/Homepage";
 import Agent from "./pages/Agent";
 import CategoricalEvents from "./pages/CategoricalEvents";
 import Admin from "./pages/Admin";
+import MapComponent from "./pages/Map";
+
 import SignIn from "./pages/Signin";
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
                 <Routes>
                 
                     <Route path="/admin" element={<Admin />} />
+                    <Route path="/map" element={<MapComponent />} />
+
                     <Route path="/" element={<Homepage />} />
                     <Route path="/agent" element={<Agent />} />
                     <Route path="/concerts" element={<CategoricalEvents type="music"/>}></Route>

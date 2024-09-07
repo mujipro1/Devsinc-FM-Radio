f9367551af1ca97ab071b6e0fdd249b7c37a227e
faizyabShah <faizyabshah0123@gmail.com>
2024-09-07 11:11:06 +0500

    categorical events and fetches

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import Agent from "./pages/Agent";
+import CategoricalEvents from "./pages/CategoricalEvents";
 
 function App() {
     return (
@@ -11,6 +12,10 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Homepage />} />
                     <Route path="/agent" element={<Agent />} />
+                    <Route path="/concerts" element={<CategoricalEvents type="concerts"/>}></Route>
+                    <Route path="/plays" element={<CategoricalEvents type="plays"/>}></Route>
+                    <Route path="/sports" element={<CategoricalEvents type="sports"/>}></Route>
+                    <Route path="/others" element={<CategoricalEvents type="other"/>}></Route>
                 </Routes>
             </div>
         </Router>

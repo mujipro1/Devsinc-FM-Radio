3de8b1d38b6c43e3aa0a38c537344cb78b63a27c
hashirscam <shkazmi.bscs21seecs@student.nust.edu.pk>
2024-09-07 10:00:58 +0500

    Agent Page Boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Homepage from './pages/Homepage'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import Agent from "./pages/Agent";
 
 function App() {
-  return (
-    <div className="App">
-      <Homepage></Homepage>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/agent" element={<Agent />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/Agent.js
@@ -0,0 +1,12 @@
+import "./Agent.css";
+// import Navbar from "../components/Navbar";
+
+const Agent = () => {
+    return (
+        <>
+            <h1>Welcome</h1>
+        </>
+    );
+};
+
+export default Agent;

62273ddc5e1d6bb5921c770fd0889cebb4188585
faizyabShah <faizyabshah0123@gmail.com>
2024-09-07 09:59:07 +0500

    Search component

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import './Homepage.css'
 import Navbar from '../components/Navbar';
+import SearchComponent from '../components/Search';
 
 
 const Homepage = () => {
     return (
         <>
         <Navbar/>
+        <SearchComponent/>
         <h1>Homepage</h1>
         </>
     )
